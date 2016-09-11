f68acb13c0214abb3e39b2cb6e58de25e8fcf548
victorkwak <victorkwak@gmail.com>
2016-09-10 23:13:23 -0500

    This is hard.

--- src/main/scala/NaiveBayesClassifierScala.scala
+++ src/main/scala/NaiveBayesClassifierScala.scala
@@ -1,53 +1,40 @@
-import org.apache.spark.mllib.classification.NaiveBayes
-import org.apache.spark.mllib.feature.{HashingTF, IDF}
-import org.apache.spark.mllib.regression.LabeledPoint
-import org.apache.spark.rdd.RDD
-import org.apache.spark.{SparkConf, SparkContext}
+import org.apache.spark.ml.classification.NaiveBayes
+import org.apache.spark.ml.feature.{HashingTF, IDF, LabeledPoint, Tokenizer}
+import org.apache.spark.sql.{DataFrame, Dataset, SparkSession}
 
 /**
   * Victor Kwak, 9/10/16
   */
 object NaiveBayesClassifierScala extends App {
   // configuration set for local running on 4 cores.
-  val configuration = new SparkConf()
-    .setAppName("Naive Bayes Spam Filter")
-    .setMaster("local[4]")
-  val sc = new SparkContext(configuration)
+  val spark = SparkSession
+    .builder()
+    .master("local[4]")
+    .appName("Naive Bayes Spam filter")
+    .getOrCreate()
 
-  val trainingData: RDD[LabeledPoint] = {
+  val trainingData = {
     val dataDirectory: String = "./Data/test/RedditData/"
     val subreddits: Seq[String] = Seq("AMA", "AskEngineers", "Economics", "Fitness", "Showerthoughts")
     val dataDirectories: Seq[String] = subreddits.map(subreddit => dataDirectory + subreddit + ".TITLE")
-    val stopWords = sc.textFile("./stopwords")
 
-    val tf = new HashingTF()
-    val idf = new IDF()
-    val data: Seq[RDD[LabeledPoint]] = {
-      //sequence of big strings (titles)
-      val input: Seq[RDD[String]] = dataDirectories
-        .map(directory => sc.textFile(directory))
-        .map(titles => titles.map(title => title.toLowerCase))
-        .map(titles => titles.subtract(stopWords))
+    val data = {
+      val input: Seq[DataFrame[String]] = dataDirectories
+        .map(directory => spark.read.text(directory).as[String])
+          .map(stringDataset => )
+//        .map(stringDataset => stringDataset.toDF("label", "sentence"))
 
-      input.foreach(titles => titles.foreach(println))
-      val labeledPoints = input
-        .zipWithIndex.map { case (stringRDD, i) => stringRDD
-        .map(line => tf.transform(line.split("\\s")))
-        .map(features => LabeledPoint(i, features))
-      }
-      labeledPoints
+      val tokenizer = new Tokenizer().setInputCol("sentence").setOutputCol("words")
+      val wordsData = input.map(stringDataset => tokenizer.transform(stringDataset))
+
+      val tf = new HashingTF().setInputCol("words").setOutputCol("rawFeatures")
+      val featureizedData = wordsData.map(stringDataFrame => tf.transform(stringDataFrame))
+      val idf = new IDF().setInputCol("rawFeatures").setOutputCol("features")
+      val idfModels = featureizedData.map(tfDataFrame => idf.fit(tfDataFrame))
+      val rescaledData = (idfModels, featureizedData).zipped
+        .map { (idfData, stringDataFrame) => idfData.transform(stringDataFrame) }
+      rescaledData
     }
-    sc.union(data)
+    data.foreach(dataframe => dataframe.select("features", "label"))
   }
-  trainingData.cache()
-
-  val randomSplit = trainingData.randomSplit(Array(0.6, 0.4))
-  val training = randomSplit(0)
-  val test = randomSplit(1)
-  val model = NaiveBayes.train(training, lambda = 1.0)
-
-  val predictionAndLabel = test.map(p => (model.predict(p.features), p.label))
-  val accuracy = 1.0 * predictionAndLabel.filter(x => x._1 == x._2).count() / test.count()
-
-  println(accuracy)
 }
